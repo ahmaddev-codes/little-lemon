b4ec94fab7d9d2ce999e5688013fd5450df146d6
Tijani Ahmad <omegabliss23@gmail.com>
2023-02-20 08:26:56 +0100

    Update: added components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "../src/components/header/Header";
+import HomePage from "./components/homepage/HomePage";
+import Specials from "./components/Specials";
+import CustomersSay from "./components/CustomersSay";
+import Chicago from "./components/Chicago";
+import BookingPage from "./components/BookingPage";
+import Footer from "../src/components/footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/specials" element={<Specials />}></Route>
+          <Route path="/testimonial" element={<CustomersSay />}></Route>
+          <Route path="/about" element={<Chicago />}></Route>
+          <Route path="/booking" element={<BookingPage />}></Route>
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
