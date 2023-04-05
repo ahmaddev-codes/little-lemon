110bc513c7b6ef1eeb4a7e7ab1a787a4f82cbf95
Tijani Ahmad <omegabliss23@gmail.com>
2023-04-05 05:02:54 +0100

    Code: added About.js component

--- src/components/homepage/about/About.js
+++ src/components/homepage/about/About.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import '../../homepage/about/About.scss';
+import owner1 from '../../../assets/owner1.png';
+import owner2 from '../../../assets/owner2.png';
 
 const About = () => {
   return (
-    <section>About</section>
-  )
-}
+    <section className="about">
+      <div className="about__content">
+        <div className="header">
+          <h1>Little Lemon</h1>
+          <h3>Chicago</h3>
+        </div>
+        <p>
+          Little Lemon is a charming neighborhood bistro
+          that serves simple food and classic cocktails
+          in a lively but casual environment.
+          The restaurant features a locally-sourced menu
+          with Daily specials.
+        </p>
+      </div>
+      <div className="about__image">
+        <img src={owner1} alt="owner 1" />
+        <img src={owner2} alt="owner 2" />
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
